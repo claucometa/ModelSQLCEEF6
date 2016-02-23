ae8d4b8eea9af0e038aaa64df7ded7a2bebc4c0e
Claucometa <claucometa@gmail.com>
2016-02-23 14:42:56 -0300

    Services

---
+++ CFsqlCe.Dal/Services/ServiceFilmTitle.cs
@@ -0,0 +1,35 @@
+﻿using CFSqlCe.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CFSqlCe.Dal.Services
+{
+    public class ServiceActor
+    {
+        CrudRepo<Actor> repo = new CrudRepo<Actor>();
+
+        public void Add()
+        {
+            var f = new Actor()
+           {
+               Name = "Angelina Julie",
+           };
+
+            var role = new ActorRole()
+            {
+                Actor = f,
+                Character = "Lara Croft",
+                Description = "Tomb Rider"
+            };
+
+            repo.Add(f);
+        }
+
+        public void Save()
+        {
+            repo.Save();
+        }
+    }
+}
